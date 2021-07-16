787931dd98ef9df3f9c33a51f46e3c4db3cee208
Osama Baker <edewanjy@hotmail.com>
2021-07-17 01:19:51 +0300

    Auth0

---
+++ src/components/BestBooks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class BestBooks extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default BestBooks
